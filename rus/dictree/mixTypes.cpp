e58a38c07a2ad76af3b4e3a7fd159d828782dc28
Андрей Коваленко < >
2016-08-05 16:41:34 +0300

    new builds

--- rus/dictree/mixTypes.cpp
+++ rus/dictree/mixTypes.cpp
@@ -1,12 +1,40 @@
+/******************************************************************************
+
+    libmorphrus - dictiorary-based morphological analyser for Russian.
+    Copyright (C) 1994-2016 Andrew Kovalenko aka Keva
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Contacts:
+      email: keva@meta.ua, keva@rambler.ru
+      Skype: big_keva
+      Phone: +7(495)648-4058, +7(926)513-2991
+
+******************************************************************************/
+
 namespace __libmorphrus__
 {
 
   unsigned char mixTypes[] =
   {
-    '\x00','\x01','\x01','\x01','\x01','\x01','\x01','\x02','\x03','\x04','\x05','\x06','\x06','\x05','\x06','\x07',
-    '\x05','\x06','\x07','\x05','\x06','\x05','\x06','\x05','\x05','\x08','\x00','\x00','\x00','\x00','\x00','\x00',
-    '\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00',
-    '\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00'
+    '\x00','\x01','\x01','\x01','\x01','\x01','\x01','\x02','\x03','\x04','\x05','\x06',
+    '\x06','\x05','\x06','\x07','\x05','\x06','\x07','\x05','\x06','\x05','\x06','\x05',
+    '\x05','\x08','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00',
+    '\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00',
+    '\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00','\x00',
+    '\x00','\x00','\x00','\x00'
   };
 
 }  // end namespace
