0eaa7ad2408d421da506135105a36d282d4d9444
Андрей Коваленко < >
2017-02-01 16:10:24 +0300

    safe word23_t definition

--- xmorph/typedefs.h
+++ xmorph/typedefs.h
@@ -10,10 +10,16 @@ namespace LIBMORPH_NAMESPACE {
 #   define  __byte_t_defined__
     typedef unsigned char   byte_t;
 # endif  // !__byte_t_defined__
+
 # if !defined( __word16_t_defined__ )
 #   define __word16_t_defined__
     typedef unsigned short  word16_t;
 # endif  // !__word16_t_defined__
+
+# if !defined( __word32_t_defined__ )
+#   define __word32_t_defined__
+    typedef unsigned int    word32_t;
+# endif  // !__word32_t_defined__
 
 # if defined( LIBMORPH_NAMESPACE )
 }
