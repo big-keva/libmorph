cbc7b244a8f9a5e10adeb7c14861da072d9482c0
Андрей Коваленко < >
2017-04-10 15:22:29 +0300

    obsolete code removed

--- tools/sweets.h
+++ tools/sweets.h
@@ -1,48 +1,28 @@
 # if !defined( __sweets_h__ )
 # define __sweets_h__
+# include <mtc/file.h>
+# include <mtc/array.h>
+# include <mtc/zarray.h>
+# include <mtc/serialize.h>
 # include <stdarg.h>
 # include <stdio.h>
 
 namespace libmorph
 {
+  using namespace mtc;
 
-  class file
+  class serialbuff
   {
-    FILE* lpfile;
-  public:     // construction
-        file( FILE* f = NULL ): lpfile( f )
-          {
-          }
-        file( const file& f ): lpfile( f.lpfile )
-          {
-            ((file&)f).lpfile = NULL;
-          }
-  file& operator = ( FILE* f )
-          {
-            if ( lpfile != NULL )
-              fclose( lpfile );
-            lpfile = f;
-              return *this;
-          }
-  file& operator = ( const file& f )
-          {
-            if ( lpfile != NULL )
-              fclose( lpfile );
-            if ( (lpfile = f.lpfile) != NULL )
-              ((file&)f).lpfile = NULL;
-            return *this;
-          }
-        ~file()
-          {
-            if ( lpfile != NULL )
-              fclose( lpfile );
-          }
-  operator FILE* ()
-          {
-            return lpfile;
-          }
+    const void* buffer;
+    size_t      length;
+
+  public:
+    serialbuff( const void* p, size_t l ): buffer( p ), length( l )
+      {}
+    template <class O> O*  Serialize( O* o ) const
+      {  return ::Serialize( o, buffer, length );  }
   };
-		
+
   inline  int   LogMessage( int nerror, const char* format, ... )
   {
     va_list   valist;
@@ -52,10 +32,10 @@ namespace libmorph
     return nerror;
   }
 
-  inline  int   LoadSource( array<char, char>&  output, const char* szfile )
+  inline  int   LoadSource( array<char>&  output, const char* szfile )
   {
-    libmorph::file  lpfile;
-    long            cbfile;
+    file  lpfile;
+    long  cbfile;
 
     if ( (lpfile = fopen( szfile, "rb" )) != NULL ) fseek( lpfile, 0, SEEK_END );
       else  return ENOENT;
