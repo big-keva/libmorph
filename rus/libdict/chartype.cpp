2b98e313753ae5ea0bd1dc39f5595e8fba1a30fd
Andrew Kovalenko aka Keva <keva@meta.ua>
2017-06-15 18:46:26 +0300

    license key

--- rus/libdict/chartype.cpp
+++ rus/libdict/chartype.cpp
@@ -17,7 +17,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    Сommercial license is available upоn request.
+    Commercial license is available upоn request.
 
     Contacts:
       email: keva@meta.ua, keva@rambler.ru
