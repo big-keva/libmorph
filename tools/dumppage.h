2107904763e310b4d83956e90d9e9fb4f60c4ce8
Andrew Kovalenko aka Keva <keva@meta.ua>
2018-06-06 17:26:40 +0300

    simplier implementation

--- tools/dumppage.h
+++ tools/dumppage.h
@@ -1,7 +1,9 @@
+# pragma once
 # if !defined( __dumppage_h__ )
 # define __dumppage_h__
-# include "sweets.h"
-# include <stdio.h>
+# include <cstdarg>
+# include <cstdio>
+# include <string>
 
 namespace libmorph
 {
@@ -14,58 +16,58 @@ namespace libmorph
 
   public:     // construction
     dumpsource( FILE* p, int maxchars = 12 ): lpfile( p ), stored( 0 ), nchars( maxchars )
+      {}
+    dumpsource* ptr() const
       {
+        return (dumpsource*)this;
       }
-    operator FILE*()
+    void  putbuff( const void* p, size_t l )
       {
-        return lpfile;
-      }
-    int putchar( char c )
-      {
-        fprintf( lpfile, "%s0x%02x", stored == 0 ? "" : (stored % nchars) == 0 ? ",\n    " : ",", (unsigned char)c );
-          ++stored;
-        return 0;
+        for ( auto pch = static_cast<const uint8_t*>( p ), end = pch + l; pch != end; ++pch, ++stored )
+          fprintf( lpfile, "%s0x%02x", stored == 0 ? "" : (stored % nchars) == 0 ? ",\n    " : ",", *pch );
       }
   };
 
   class BinaryDumper
   {
-    const char* pszdir;
-    const char* nspace;
-    const char* szname;
-    const char* header;
-    file        lpfile;
-    int         nerror;
+    std::string outdir;
+    std::string nspace;
+    std::string szname;
+    std::string header;
+    FILE*       lpfile;
 
   public:     // construction
-    BinaryDumper(): pszdir( NULL ), nspace( NULL ), szname( NULL ), header( NULL ), nerror( 0 )
-      {
-      }
+    BinaryDumper(): lpfile( nullptr )
+      {}
    ~BinaryDumper()
       {
-        if ( (FILE*)lpfile != NULL && nspace != NULL && *nspace != '\0' )
-          fprintf( lpfile, "\n"  "}  // end namespace\n\n" );
+        if ( lpfile != nullptr )
+        {
+          if ( nspace != "" )
+            fprintf( lpfile, "\n"  "}  // end namespace\n\n" );
+          fclose( lpfile );
+        }
       }
 
-    BinaryDumper& OutDir( const char* d )     {  pszdir = d;  return *this;  }
-    BinaryDumper& Header( const char* h )     {  header = h;  return *this;  }
-    BinaryDumper& Namespace( const char* n )  {  nspace = n;  return *this;  }
-    BinaryDumper& Output( const char* f )     {  szname = f;  return *this;  }
-
-    operator int()  {  return nerror;  }
+    BinaryDumper& OutDir( const char* d )           {  outdir = d;  return *this;  }
+    BinaryDumper& OutDir( const std::string& d )    {  outdir = d;  return *this;  }
+    BinaryDumper& Header( const char* h )           {  header = h;  return *this;  }
+    BinaryDumper& Header( const std::string& h )    {  header = h;  return *this;  }
+    BinaryDumper& Namespace( const char* n )        {  nspace = n;  return *this;  }
+    BinaryDumper& Namespace( const std::string& n ) {  nspace = n;  return *this;  }
+    BinaryDumper& Output( const char* f )           {  szname = f;  return *this;  }
+    BinaryDumper& Output( const std::string& f )    {  szname = f;  return *this;  }
 
   public:
-    BinaryDumper& Type( const char* format, ... )
+    BinaryDumper& Print( const char* format, ... )
       {
         FILE*   pf;
 
-        if ( (pf = OpenFile( NULL )) != NULL )
+        if ( (pf = OpenFile( nullptr )) != nullptr )
         {
           va_list valist;
           va_start( valist, format );
-
-          vfprintf( pf, format, valist );
-
+            vfprintf( pf, format, valist );
           va_end( valist );
         }
         return *this;
@@ -78,10 +80,7 @@ namespace libmorph
         if ( (pf = OpenFile( vaname )) != NULL )
         {
           fprintf( pf, "  unsigned char %s[] =\n"  "  {\n"  "    ", vaname );
-            {
-              dumpsource  dumpsc( lpfile );
-                serial.Serialize( &dumpsc );
-            }
+            serial.Serialize( dumpsource( pf ).ptr() );
           fprintf( pf, "\n"  "  };\n" );
         }
         return *this;
@@ -89,52 +88,43 @@ namespace libmorph
   protected:  // helper
     FILE* OpenFile( const char* vaname )
       {
-        if ( (FILE*)lpfile == NULL )
+        if ( lpfile == nullptr )
         {
-          const char* thedir = pszdir != NULL ? pszdir : "";
-          char        szpath[0x400];
+          std::string stpath;
           const char* sslash = "";
 
-          if ( *thedir != '\0' && strchr( "/\\", thedir[strlen( thedir ) - 1] ) == NULL )
+          if ( outdir != "" && strchr( "/\\", outdir.back() ) == nullptr )
             sslash = "/";
-          sprintf( szpath, "%s%s%s.cpp", thedir, sslash, szname != NULL ? szname : vaname );
 
-          if ( (lpfile = fopen( szpath, "wt" )) == NULL )
-          {
-            nerror = LogMessage( ENOENT, "Could not create file \'%s\'!\n", szpath );
-            return NULL;
-          }
+          stpath = outdir + sslash + (szname != "" ? szname : vaname) + ".cpp";
+
+          if ( (lpfile = fopen( stpath.c_str(), "wt" )) == NULL )
+            throw std::runtime_error( "coult not create file '" + stpath + "'" );
 
         // print header if available
-          if ( header != NULL && *header != '\0' )
-            fprintf( lpfile, "%s\n", header );
+          if ( header != "" )
+            fprintf( lpfile, "%s\n", header.c_str() );
 
         // print namespace if available
-          if ( nspace != NULL && *nspace != '\0' )
-            fprintf( lpfile, "namespace %s\n"  "{\n"  "\n", nspace );
+          if ( nspace != "" )
+            fprintf( lpfile, "namespace %s\n"  "{\n"  "\n", nspace.c_str() );
         }
         return lpfile;
       }
   };
 
-  template <class serializable>
-  int       DumpBinary( const char* pszdir, const char*   nspace,
-                        const char* vaname, serializable& serial )
+  template <class dirstr, class nspstr, class varstr, class serializable>
+  int       DumpBinary( dirstr pszdir, nspstr nspace, varstr vaname, serializable& serial )
   {
     return BinaryDumper().OutDir( pszdir ).Namespace( nspace ).Dump( vaname, serial );
   }
 
 } // end 'libmorph' namespace
 
-inline  libmorph::dumpsource* Serialize( libmorph::dumpsource* o, char c )
-{
-  return o != NULL && o->putchar( c ) == 0 ? o : NULL;
-}
-
 inline  libmorph::dumpsource* Serialize( libmorph::dumpsource* o, const void* p, size_t l )
 {
-  for ( ; o != NULL && l-- > 0; p = 1 + (char*)p )
-    o = Serialize( o, *(char*)p );
+  if ( o != nullptr )
+    o->putbuff( p, l );
   return o;
 }
 
