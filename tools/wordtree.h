d93464baac7551bba26e1fd6e2802a487f44d2f8
Andrew Kovalenko aka Keva <keva@meta.ua>
2018-07-24 18:20:54 +0300

    using local serialize.h

--- tools/wordtree.h
+++ tools/wordtree.h
@@ -1,7 +1,7 @@
 # pragma once
 # if !defined( __libmorph_wordtree_h__ )
 # define  __libmorph_wordtree_h__
-# include <mtc/serialize.h>
+# include <tools/serialize.h>
 # include <cassert>
 # include <memory>
 # include <vector>
