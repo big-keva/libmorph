2ba19e3f56f578285025c2f4e4f5552f949a05f6
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2022-04-04 20:33:08 +0300

    license updated

--- ukr/libdict/mlmadefs.h
+++ ukr/libdict/mlmadefs.h
@@ -17,6 +17,8 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+    Commercial license is available upоn request.
+
     Contacts:
       email: keva@meta.ua, keva@rambler.ru
       Skype: big_keva
