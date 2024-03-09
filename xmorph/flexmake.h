6235730be6a846dea1083aa424add8444ffc5678
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2024-03-09 19:13:06 +0300

    readability refactoring

--- xmorph/flexmake.h
+++ xmorph/flexmake.h
@@ -1,83 +1,32 @@
-# if !defined( _flexmake_h_ )
-# define _flexmake_h_
+# if !defined( __libmorph_flexmake_h__ )
+# define __libmorph_flexmake_h__
 # include <namespace.h>
-# include "scandict.h"
-# include "mlmadefs.h"
-# include <string.h>
+# include "typedefs.h"
 
 namespace LIBMORPH_NAMESPACE
 {
-  class  stringCollect
-  {
-    byte_t*       output;
-    const byte_t* prefix;
-    size_t        ccpref;
-    int           fcount;
 
-  public:     // collector
-    stringCollect( byte_t* o, const byte_t* p, size_t l ): output( o ), prefix( p ), ccpref( l ), fcount( 0 )
-      {}
-    void  AddStr( const byte_t* addstr, size_t cchstr )
-      {
-        size_t        cbcopy;
-        const byte_t* szcopy;
-
-        for ( szcopy = prefix, cbcopy = ccpref; cbcopy-- > 0; )
-          *output++ = *szcopy++;
-
-        for ( ++fcount; cchstr-- > 0; )
-          *output++ = *addstr++;
-
-        *output++ = '\0';
-      }
-    int   GetLen() const
-      {
-        return fcount;
-      }
-  };
-
-  struct  doCollectFlex
-  {
-    doCollectFlex( stringCollect& o, const byte_t* t, word16_t g, byte_t f ):
-        output( o ), grinfo( g ), bflags( f )
-      {  (void)t;  }
-
-  public:     // functor
-    int   operator () ( const byte_t* pflist, const byte_t* ptrace, unsigned ltrace ) const
-      {
-        int   ngrams = *pflist++;
-      
-        while ( ngrams-- > 0 )
-        {
-          word16_t  cginfo = getword16( pflist );
-          byte_t    cflags = *pflist++;
-        
-          if ( cginfo == grinfo && (bflags & cflags) != 0 )
-            output.AddStr( ptrace, ltrace );
-        }
-        return 0;
-      }
-
-  protected:  // variables
-    stringCollect&  output;
-    const word16_t  grinfo;
-    const byte_t    bflags;
-
-  };
-  
-  inline  int   BuildFlexSet(       byte_t* pszout,
-                              const byte_t* ptable,
-                              const byte_t* prefix, size_t  ccpref,
-                              word16_t      grinfo, byte_t  bother )
-  {
-    byte_t        atrack[0x100];
-    stringCollect output( pszout, prefix, ccpref );
-
-    return RecursGetTrack<byte_t, int>( doCollectFlex( output, atrack, grinfo, bother ), ptable, atrack, 0 ) >= 0 ?
-      output.GetLen() : -1;
-  }
+/*
+ * BuildFlexSet(
+ *   [out] output buffer,
+ *   [in] flexion table,
+ *   [in] grammatical description,
+ *   [in] default flexion prefix
+ *
+ * Заполняет массив output вариантами окончаний для переданной грамматики, дополнив их
+ * переданным префиксом и разделив нулевым символом.
+ *
+ * Возвращает количество построенных строк.
+ *
+ * Проверка размера массива output не делается - считается, что в статически выверенном словаре
+ * с предопределённой системой окончаний всегда можно предсказать достаточный его размер.
+ */
+  int   BuildFlexSet(
+    byte_t*         output,
+    const byte_t*   ptable,
+    const flexinfo& inflex,
+    const fragment& prefix = { nullptr, 0 } );
 
 } // end namespace
 
-# endif // _flexmake_h_
-
+# endif // !__libmorph_flexmake_h__
