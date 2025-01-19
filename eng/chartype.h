914f663e7099eb33739ba383ce5a84392344d881
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2025-01-19 11:33:53 +0300

    common character case conversions

---
+++ eng/chartype.h
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+    libmorpheng - dictiorary-based morphological analyser for English.
+    Copyright (C) 1994-2016 Andrew Kovalenko aka Keva
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Commercial license is available upоn request.
+
+    Contacts:
+      email: keva@meta.ua, keva@rambler.ru
+      Skype: big_keva
+      Phone: +7(495)648-4058, +7(926)513-2991
+
+******************************************************************************/
+
+namespace libmorph {
+namespace eng {
+
+  extern const unsigned char toLoCaseMatrix[256];
+  extern const unsigned char toUpCaseMatrix[256];
+
+}}
