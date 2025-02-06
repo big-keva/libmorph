3a93603569d1282167c644ee6a8416daa7b1fae2
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2025-02-06 14:12:55 +0300

    license headers added

---
+++ eng/license.h
@@ -0,0 +1,37 @@
+# if !defined( __libmorph_eng_license_h__ )
+# define __libmorph_eng_license_h__
+
+# define libmorph_eng_license \
+  "/******************************************************************************\n" \
+  "\n"                                                                                \
+  "libmorpheng - dictionary-based morphological analyser for English.\n"              \
+  "\n"                                                                                \
+  "Copyright (C) 1994-2016 Andrew Kovalenko aka Keva\n"                               \
+  "\n"                                                                                \
+  "Permission is hereby granted, free of charge, to any person obtaining a copy\n"    \
+  "of this software and associated documentation files (the \"Software\"), to deal\n" \
+  "in the Software without restriction, including without limitation the rights\n"    \
+  "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"       \
+  "copies of the Software, and to permit persons to whom the Software is\n"           \
+  "furnished to do so, subject to the following conditions:\n"                        \
+  "\n"                                                                                \
+  "The above copyright notice and this permission notice shall be included in all\n"  \
+  "copies or substantial portions of the Software.\n"                                 \
+  "\n"                                                                                \
+  "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"    \
+  "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"        \
+  "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"     \
+  "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"          \
+  "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"   \
+  "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"   \
+  "SOFTWARE.\n"                                                                       \
+  "\n"                                                                                \
+  "Commercial license is available upon request.\n"                                   \
+  "\n"                                                                                \
+  "Contacts:\n"                                                                       \
+  "  email: keva@meta.ua, keva@rambler.ru\n"                                          \
+  "  Phone: +7(495)648-4058, +7(926)513-2991, +7(707)129-1418\n"                      \
+  "\n"                                                                                \
+  "******************************************************************************/\n"
+
+# endif  // __libmorph_eng_license_h__
