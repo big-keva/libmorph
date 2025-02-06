62ffe25d8f5e64787e0d18346ff190e144125f28
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2025-02-06 14:01:46 +0300

    license headers added

--- xmorph/scandict.h
+++ xmorph/scandict.h
@@ -1,3 +1,34 @@
+/******************************************************************************
+
+    libmorph - morphological analysers.
+
+    Copyright (C) 1994-2016 Andrew Kovalenko aka Keva
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    Commercial license is available upon request.
+
+    Contacts:
+      email: keva@meta.ua, keva@rambler.ru
+      Phone: +7(495)648-4058, +7(926)513-2991, +7(707)129-1418
+
+******************************************************************************/
 # if !defined( __libmorph_scandict_h__ )
 # define  __libmorph_scandict_h__
 # include "typedefs.h"
