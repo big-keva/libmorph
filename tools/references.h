a1b30164889a590dd949460141d0a8ee48b8f12b
Andrey.Kovalenko <andrey.kovalenko@myoffice.team>
2025-02-06 14:30:26 +0300

    license dates fix

--- tools/references.h
+++ tools/references.h
@@ -2,7 +2,7 @@
 
     libmorph - morphological analysers.
 
-    Copyright (C) 1994-2016 Andrew Kovalenko aka Keva
+    Copyright (C) 1994-2025 Andrew Kovalenko aka Keva
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
