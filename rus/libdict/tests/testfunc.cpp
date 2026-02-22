2e85ece98767598920b0f59fa33d230a4f4c98ac
Keva <keva@rambler.ru>
2026-02-22 20:01:52 +0300

    contacts fix; api changes reflected

--- rus/libdict/tests/testfunc.cpp
+++ rus/libdict/tests/testfunc.cpp
@@ -1,8 +1,8 @@
 /******************************************************************************
 
-    libmorphrus - dictiorary-based morphological analyser for Russian.
+    libmorphrus - dictionary-based morphological analyser for Russian.
 
-    Copyright (C) 1994-2025 Andrew Kovalenko aka Keva
+    Copyright (c) 1994-2026 Andrew Kovalenko aka Keva
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@
     Commercial license is available upon request.
 
     Contacts:
-      email: keva@meta.ua, keva@rambler.ru
+      email: keva@rambler.ru
       Phone: +7(495)648-4058, +7(926)513-2991, +7(707)129-1418
 
 ******************************************************************************/
@@ -38,6 +38,10 @@
 # include <cstdio>
 # include <mtc/test-it-easy.hpp>
 # include <thread>
+
+const lexeme_t  lMETLA = 61579;
+const lexeme_t  lMOSKVA = 181292;
+const lexeme_t  lKTONIBUD = 176132;
 
 struct  StrMatch
 {
@@ -54,12 +58,9 @@ struct  LexMatch
 class MatchSet: public std::vector<LexMatch>, public IMlmaMatch
 {
 public:
-  auto  ptr() const -> IMlmaMatch*  {  return (IMlmaMatch*)this;  }
-
-public:
   int   MLMAPROC  Attach() override {  return 1;  }
   int   MLMAPROC  Detach() override {  return 1;  }
-  int   MLMAPROC  RegisterLexeme( lexeme_t nlexid, int  nmatch, const SStrMatch*  pmatch ) override
+  int   MLMAPROC  AddLexeme( lexeme_t nlexid, int  nmatch, const SStrMatch*  pmatch ) override
     {
       push_back( { nlexid, {} } );
 
@@ -70,7 +71,7 @@ public:
     }
 
 public:
-  auto  ToString( unsigned codepage = codepages::codepage_utf8 ) -> std::string
+  auto  ToString() -> std::string
   {
     auto  lst = std::vector<std::string>();
     auto  out = std::string();
@@ -87,26 +88,17 @@ public:
     for ( size_t i = 0; i != lst.size(); ++i )
       out += (i != 0 ? "/" : "") + lst[i];
 
-    return codepages::mbcstombcs( codepage, codepages::codepage_1251, out );
+    return out;
   }
 };
 
-auto  FindMatch( IMlmaMb* mlma, const char* ptpl ) -> MatchSet
+TestItEasy::RegisterFunc  testmorphrusmb( []()
 {
-  MatchSet  match;
-
-  mlma->FindMatch( match.ptr(), ptpl );
-
-  return match;
-}
-
-TestItEasy::RegisterFunc  testmorphrus( []()
-{
-  TEST_CASE( "morph/rus" )
+  TEST_CASE( "morph/rus/IMlmaMb" )
   {
     SECTION( "morphological analyser API may be created with different mbcs codepages" )
     {
-      IMlmaMb*  mlma = nullptr;
+      IMlmaMbXX*  mlma = nullptr;
 
       SECTION( "valid string keys give access to named API" )
       {
@@ -465,41 +457,41 @@ TestItEasy::RegisterFunc  testmorphrus( []()
       }
       SECTION( "FindMatch" )
       {
-        auto  ms = MatchSet();
-
         SECTION( "it checks arguments" )
         {
           SECTION( "called with invalid arguments, it returns ARGUMENT_FAILED " )
-            {  REQUIRE( mlma->FindMatch( nullptr, nullptr ) == ARGUMENT_FAILED );  }
+            {  REQUIRE( mlma->FindMatch( nullptr, nullptr, 0 ) == ARGUMENT_FAILED );  }
           SECTION( "with too long string, it returns WORDBUFF_FAILED" )
-            {  REQUIRE( mlma->FindMatch( ms.ptr(), std::string( 257, 'a' ).c_str() ) == WORDBUFF_FAILED );  }
+            {  REQUIRE( mlma->FindMatch( (IMlmaMatch*)-1, std::string( 257, 'a' ).c_str() ) == WORDBUFF_FAILED );  }
         }
         SECTION( "it loads forms by template" )
         {
+          MatchSet  ms;
+
           SECTION( "? matches any single character:" )
           {
             SECTION( "∙ in non-flective words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "вдн?" ) );
-                REQUIRE( ms.size() == 1 );
-                REQUIRE( ms.ToString() == "вднх" );
-                if ( REQUIRE( ms.front().aforms.size() == 1 ) )
-                {
-                  REQUIRE( ms.front().aforms.front().id == (uint8_t)-1 );
-                }
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "вд?х" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &ms, "вдн?" ) );
+              REQUIRE( ms.size() == 1 );
+              REQUIRE( ms.ToString() == "вднх" );
+              if ( REQUIRE( ms.front().aforms.size() == 1 ) )
+              {
+                REQUIRE( ms.front().aforms.front().id == (uint8_t)-1 );
+              }
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "вд?х" ) );
                 REQUIRE( ms.size() == 2 );    // вднх/вдох
                 REQUIRE( ms.ToString() == "вднх/вдох" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "в?нх" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "в?нх" ) );
                 REQUIRE( ms.size() == 2 );    // вднх/вдох
                 REQUIRE( ms.ToString() == "вднх/вснх" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "?днх" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "?днх" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "вднх" );
             }
             SECTION( "∙ in stems of flective words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "с?баки" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "с?баки" ) );
               if ( REQUIRE( ms.size() == 1 ) )
               {
                 REQUIRE( ms.ToString() == "собаки" );
@@ -512,7 +504,7 @@ TestItEasy::RegisterFunc  testmorphrus( []()
             }
             SECTION( "∙ in flexions of flective words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "собака?" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "собака?" ) );
                 if ( REQUIRE( ms.size() == 1 ) )
                 {
                   REQUIRE( ms.ToString() == "собакам/собаках" );
@@ -526,28 +518,28 @@ TestItEasy::RegisterFunc  testmorphrus( []()
             }
             SECTION( "∙ in flexions of words with character swaps;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "сосенка?" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "сосенка?" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "сосенкам/сосенках" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "сосено?" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "сосено?" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "сосенок" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "сосен?к" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "сосен?к" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "сосенок" );
             }
             SECTION( "∙ in postfixes;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "кто-н?будь" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "кто-н?будь" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "кто-нибудь" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "кого-н?будь" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "кого-н?будь" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "кого-нибудь" );
             }
             SECTION( "∙ in postfixed words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "кт?-нибудь" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "кт?-нибудь" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "кто-нибудь" );
             }
@@ -556,58 +548,39 @@ TestItEasy::RegisterFunc  testmorphrus( []()
           {
             SECTION( "∙ in non-flective words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "вдн*" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "вдн*" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "вднх" );
             }
             SECTION( "∙ in flective words;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "дрызгаются*" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "дрызгаются*" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "дрызгаются" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "дрызг*тся" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "дрызг*тся" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "дрызгается/дрызгаются" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "дрызг*аются" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "дрызг*аются" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "дрызгаются" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "пятницей*" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "пятницей*" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "пятницей" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "?рызг*аются" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "?рызг*аются" ) );
                 REQUIRE( ms.size() == 2 );
                 REQUIRE( ms.ToString() == "брызгаются/дрызгаются" );
             }
             SECTION( "∙ in postfixes;" )
             {
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "кто-ниб*" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "кто-ниб*" ) );
                 REQUIRE( ms.size() == 1 );
                 REQUIRE( ms.ToString() == "кто-нибудь" );
-              REQUIRE_NOTHROW( ms = FindMatch( mlma, "кто-**" ) );
+              REQUIRE_NOTHROW( mlma->FindMatch( &(ms = MatchSet()), "кто-**" ) );
                 REQUIRE( ms.size() == 3 );
                 REQUIRE( ms.ToString() == "кто-либо/кто-нибудь/кто-то" );
             }
           }
         }
-      }
-      SECTION( "EnumWords" )
-      {
-/*
-//        FindMatches( mlma, "собака*" );
-//        FindMatches( mlma, "мелоч*" );
-//        FindMatches( mlma, "ху*" );
-        auto  lset_1 = FindMatches( mlma, "мелоч*" );
-        auto  lset_2 = LemmatizeIt( mlma, "мелочь" );
-        auto  lset_3 = lset_1 & lset_2;
-
-        REQUIRE( !lset_1.empty() );
-        REQUIRE( !lset_2.empty() );
-        REQUIRE( !lset_3.empty() );*/
-        /*
-    MLMA_METHOD( EnumWords )( MLMA_THIS
-                              IMlmaEnum*      pienum,
-                              const char*     pszstr, size_t    cchstr ) MLMA_PURE;
-        */
       }
     }
     if ( false )
@@ -645,6 +618,189 @@ TestItEasy::RegisterFunc  testmorphrus( []()
 
               s += (ccform + 1);
             }
+          }
+        }
+      }
+    }
+  }
+} );
+
+TestItEasy::RegisterFunc  testmorphrusws( []()
+{
+  TEST_CASE( "morph/rus/IMlmaWc" )
+  {
+    SECTION( "morphological analyser API may be created" )
+    {
+      IMlmaWcXX*  mlma = nullptr;
+
+      if ( REQUIRE_NOTHROW( mlmaruLoadWcAPI( &mlma ) ) && REQUIRE( mlma != nullptr ) )
+      {
+        SECTION( "CheckWord" )
+        {
+          SECTION( "it accepts correct words and rejects errors" )
+          {
+            REQUIRE( mlma->CheckWord( u"проверка", mtc::w_strlen( u"проверка" ), 0 ) == 1 );
+            REQUIRE( mlma->CheckWord( u"ывал", mtc::w_strlen( u"ывал" ), 0 ) == 0 );
+            REQUIRE( mlma->CheckWord( u"проверкаа", mtc::w_strlen( u"проверкаа" ), 0 ) == 0 );
+          }
+          SECTION( "with invalid capitalization, it accepts or rejects words depending on options" )
+          {
+            REQUIRE( mlma->CheckWord( u"ПрОвЕрКа", mtc::w_strlen( u"ПрОвЕрКа" ), 0 ) == 0 );
+            REQUIRE( mlma->CheckWord( u"ПрОвЕрКа", mtc::w_strlen( u"ПрОвЕрКа" ), sfIgnoreCapitals ) == 1 );
+          }
+          SECTION( "called with unknown length, it detects it" )
+          {
+            REQUIRE( mlma->CheckWord( u"воздвигнуть" ) == 1 );
+            REQUIRE( mlma->CheckWord( u"вОздвигнуть" ) == 0 );
+            REQUIRE( mlma->CheckWord( u"вОздвигнуть", (size_t)-1, sfIgnoreCapitals ) == 1 );
+          }
+        }
+        SECTION( "GetWdInfo" )
+        {
+          unsigned char wdinfo;
+
+          SECTION( "it returns the technical part-of-speach for any lexeme" )
+          {
+            REQUIRE( mlma->GetWdInfo( &wdinfo, 128 ) != 0 );
+              REQUIRE( wdinfo == 1 );
+            REQUIRE( mlma->GetWdInfo( 128 ) == 1 );
+          }
+          SECTION( "for invalid lexemes, it returns 0" )
+          {
+            REQUIRE( mlma->GetWdInfo( 127 ) == 0 );
+          }
+        }
+        SECTION( "Lemmatize" )
+        {
+          SLemmInfoW  alemms[0x20];
+          widechar    aforms[0xf0];
+          SGramInfo   agrams[0x40];
+          auto        lemmas = decltype( mlma->Lemmatize( u"" ) )();
+
+          SECTION( "parameters check is performed" )
+          {
+            REQUIRE( mlma->Lemmatize( u"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0x40,
+              nullptr, 0, nullptr, 0, nullptr, 0, 0 ) == WORDBUFF_FAILED );
+          }
+          SECTION( "unknown words are not lemmatized" )
+          {
+            REQUIRE( mlma->Lemmatize( u"f", 1, nullptr, 0, nullptr, 0, nullptr, 0, 0 ) == 0 );
+            REQUIRE( mlma->Lemmatize( u"f", -1, nullptr, 0, nullptr, 0, nullptr, 0, 0 ) == 0 );
+            REQUIRE( mlma->Lemmatize( u"aaa", -1, nullptr, 0, nullptr, 0, nullptr, 0, 0 ) == 0 );
+            REQUIRE( mlma->Lemmatize( u"простойй", -1, nullptr, 0, nullptr, 0, nullptr, 0, 0 ) == 0 );
+
+            REQUIRE( mlma->Lemmatize( u"f", alemms, aforms, agrams ) == 0 );
+            REQUIRE( mlma->Lemmatize( u"ааа", alemms, aforms, agrams ) == 0 );
+            REQUIRE( mlma->Lemmatize( u"простойй", alemms, aforms, agrams ) == 0 );
+          }
+          SECTION( "non-flective words produce same forms" )
+          {
+            REQUIRE( mlma->Lemmatize( u"суахили", {}, aforms ) == 2 );
+              REQUIRE( mtc::widestr( aforms ) == u"суахили" );
+
+            REQUIRE( mlma->Lemmatize( u"Тарту", {}, aforms ) == 1 );
+              REQUIRE( mtc::widestr( aforms ) == u"Тарту" );
+          }
+          SECTION( "lemmatization without grammatical info builds forms" )
+          {
+            REQUIRE( mlma->Lemmatize( u"простой", -1, nullptr, 0, aforms, 0xf0, nullptr, 0, 0 ) == 3 );
+            REQUIRE( mlma->Lemmatize( u"простой", {}, aforms ) == 3 );
+          }
+          SECTION( "lemmatization without strings gets lemmas" )
+          {
+            REQUIRE( mlma->Lemmatize( u"простой", -1, alemms, 0x20, nullptr, 0, nullptr, 0, 0 ) == 3 );
+            REQUIRE( mlma->Lemmatize( u"простой", alemms ) == 3 );
+          }
+          SECTION( "complete lemmatization builds strings, gets lemmas and grammatical descriptions" )
+          {
+            REQUIRE( mlma->Lemmatize( u"простой", alemms, aforms, agrams ) == 3 );
+            REQUIRE( mlma->Lemmatize( u"простой" ).size() == 3 );
+          }
+          SECTION( "by default it is case-sensitive" )
+          {
+            REQUIRE( mlma->Lemmatize( u"киев" ).size() == 1 );
+            REQUIRE( mlma->Lemmatize( u"Киев" ).size() == 2 );
+          }
+          SECTION( "with sfIgnoreCapitals it is case-insensitive" )
+          {
+            REQUIRE( mlma->Lemmatize( u"киев", sfIgnoreCapitals ).size() == 2 );
+          }
+          SECTION( "plural nouns are lemmatized to plural nominative" )
+          {
+            if ( REQUIRE_NOTHROW( lemmas = mlma->Lemmatize( u"ножницами" ) ) )
+              if ( REQUIRE( lemmas.size() == 1 ) )
+                REQUIRE( lemmas.front().normal == u"ножницы" );
+          }
+          SECTION( "dictionary form is built in minimal correct capitalization" )
+          {
+            if ( REQUIRE( (lemmas = mlma->Lemmatize( u"Москвой" )).size() == 1 ) )
+              REQUIRE( lemmas.front().normal == u"Москва" );
+            if ( REQUIRE( (lemmas = mlma->Lemmatize( u"санкт-петербург", sfIgnoreCapitals )).size() == 1 ) )
+              REQUIRE( lemmas.front().normal == u"Санкт-Петербург" );
+            if ( REQUIRE( (lemmas = mlma->Lemmatize( u"комсомольск-на-амуре", sfIgnoreCapitals )).size() == 1 ) )
+              REQUIRE( lemmas.front().normal == u"Комсомольск-на-Амуре" );
+          }
+          SECTION( "cxx wrapper also provides lexemes" )
+          {
+            if ( REQUIRE( (lemmas = mlma->Lemmatize( u"простой" )).size() == 3 ) )
+            {
+              REQUIRE( lemmas[0].normal == u"простой" );
+              REQUIRE( lemmas[1].normal == u"простоять" );
+              REQUIRE( lemmas[2].normal == u"простой" );
+            }
+          }
+        }
+        SECTION( "BuildForm" )
+        {
+          auto  aforms = decltype(mlma->BuildForm( 0, 0 ))();
+
+          SECTION( "it builds forms for lexemes" )
+          {
+            if ( REQUIRE( (aforms = mlma->BuildForm( lMETLA, 0 )).size() == 1 ) )
+              REQUIRE( aforms.front() == u"метла" );
+          }
+        }
+        SECTION( "FindForms" )
+        {
+          auto  aforms = decltype(mlma->BuildForm( 0, 0 ))();
+
+          SECTION( "it builds forms for string words" )
+          {
+            if ( REQUIRE( (aforms = mlma->FindForms( u"простой", 0 )).size() == 3 ) )
+            {
+              REQUIRE( aforms[0] == u"простой" );
+              REQUIRE( aforms[1] == u"простоять" );
+              REQUIRE( aforms[2] == u"простой" );
+            }
+          }
+        }
+        SECTION( "CheckHelp" )
+        {
+          widechar  szhelp[0x100];
+
+          SECTION( "for dictionary templates it returns the list of characters" )
+          {
+            REQUIRE( mlma->CheckHelp( u"м?жичок" ) == u"у" );
+            REQUIRE( mlma->CheckHelp( szhelp, u"слоновник*" ) == 6 );
+          }
+        }
+        SECTION( "FindMatch" )
+        {
+          std::vector<mtc::widestr> matches;
+
+          SECTION( "lambda callback may be used" )
+          {
+            REQUIRE_NOTHROW( mlma->FindMatch( u"м?жичок", [&]( lexeme_t, int, const SStrMatch* p ) -> int
+              {
+                auto  addstr = mtc::widestr( p->ws, p->cc );
+
+                if ( std::find( matches.begin(), matches.end(), addstr ) == matches.end() )
+                  matches.push_back( std::move( addstr ) );
+
+                return 0;
+              } ) );
+            if ( REQUIRE( matches.size() == 1 ) )
+              REQUIRE( matches.front() == u"мужичок" );
           }
         }
       }
