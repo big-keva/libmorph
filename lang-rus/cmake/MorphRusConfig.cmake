cc9f217d49a79b9b552aa7fae8cc4850ff16966b
Keva <keva@rambler.ru>
2026-03-22 20:26:50 +0300

    fix library linkage names

--- lang-rus/cmake/MorphRusConfig.cmake
+++ lang-rus/cmake/MorphRusConfig.cmake
@@ -1,14 +1,13 @@
-find_path(MorphRus_INCLUDE_DIR
+find_path(MorphRus_INCLUDE
     NAMES rus.h
     PATHS /usr/local/include/libmorph /usr/include/libmorph)
 
-find_library(MorphRus_LIBRARY
-    NAMES morphrus fuzzyrus
-    PATHS /usr/local/lib /usr/lib)
+find_library(MorphRus_LIB NAMES morphrus PATHS /usr/local/lib /usr/lib)
+find_library(FuzzyRus_LIB NAMES fuzzyrus PATHS /usr/local/lib /usr/lib)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(MorphRus
-    REQUIRED_VARS MorphRus_LIBRARY MorphRus_INCLUDE_DIR)
+    REQUIRED_VARS MorphRus_LIB FuzzyRus_LIB MorphRus_INCLUDE)
 
-mark_as_advanced(MorphRus_INCLUDE_DIR MorphRus_LIBRARY)
+mark_as_advanced(MorphRus_INCLUDE MorphRus_LIB FuzzyRus_LIB)
