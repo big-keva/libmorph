15bebd24a7647e37d89527a45a5d52638bad5190
Keva <keva@rambler.ru>
2026-03-30 18:54:33 +0300

    build english packages

--- packages/build-packages.sh
+++ packages/build-packages.sh
@@ -6,15 +6,19 @@ cmake -B build -S ../ && cmake --build build -j$(nproc)
 #
 # install all Components
 #
-cmake --install build --component RusRtm --prefix ./morphrus/usr
 cmake --install build --component ApiDev --prefix ./morphapi-dev/usr
+cmake --install build --component RusRtm --prefix ./morphrus/usr
 cmake --install build --component RusDev --prefix ./morphrus-dev/usr
+
+cmake --install build --component EngRtm --prefix ./morpheng/usr
+cmake --install build --component EngDev --prefix ./morpheng-dev/usr
 
 #
 # read the versions
 #
 API_VER=$(cat "../API_VER")
 RUS_VER=${API_VER}.$(cat "../lang-rus/dict/VERSION")
+ENG_VER=${API_VER}.$(cat "../lang-eng/dict/VERSION")
 
 #
 # create morphapi-dev
@@ -33,9 +37,25 @@ cat DEBIAN/morphrus.control | sed "s/@VERSION@/${RUS_VER}/g" > morphrus/DEBIAN/c
 dpkg-deb --build ./morphrus "./morphrus_${RUS_VER}_amd64.deb"
 
 #
-# create morphapi-dev
+# create morphrus-dev
 #
 echo "building morphrus-dev..."
 mkdir -p morphrus-dev/DEBIAN
 cat DEBIAN/morphrus-dev.control | sed "s/@VERSION@/${RUS_VER}/g" > morphrus-dev/DEBIAN/control
 dpkg-deb --build ./morphrus-dev "./morphrus-dev_${RUS_VER}_amd64.deb"
+
+#
+# create morpheng
+#
+echo "building morpheng..."
+mkdir -p morpheng/DEBIAN
+cat DEBIAN/morpheng.control | sed "s/@VERSION@/${ENG_VER}/g" > morpheng/DEBIAN/control
+dpkg-deb --build ./morpheng "./morpheng_${ENG_VER}_amd64.deb"
+
+#
+# create morphrus-dev
+#
+echo "building morpheng-dev..."
+mkdir -p morpheng-dev/DEBIAN
+cat DEBIAN/morpheng-dev.control | sed "s/@VERSION@/${ENG_VER}/g" > morpheng-dev/DEBIAN/control
+dpkg-deb --build ./morpheng-dev "./morpheng-dev_${ENG_VER}_amd64.deb"
